57a222c5f6545a12ba37cbd2a46311bdde83a6c3
maarufbussri <134280908+maarufbussri@users.noreply.github.com>
2023-08-25 13:56:00 +0800

    Create fibonacci_more_than_1.c
    Generate Fibonacci Numbers for n>1 or n>=2.
    
    Fibonacci recurrence relation defined as;
    F(n) = F(n-1) + F(n-2), for n> 1 or n>=2
    
    The sequence start with;
    F(0) = 0 and F(1) = 1

---
+++ fibonacci_more_than_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//Fibonacci Sequence
+
+int main()
+{
+	int n = 2; //n>1 or n>= 2
+	int m = 10; //Number of terms
+	int F0 = 0; //F(0) = 0, initial value n<2 or n<=1
+	int F1 = 1; //F(1) = 1, initial value n<2 or n<=1
+	int Fn; //F(n)
+	int Fn_2; //F(n-2)
+	int Fn_1; //F(n-1)
+
+	Fn_2 = F0; //F(n-2) = F(0), initial condition n = 2
+	Fn_1 = F1; //F(n-1) = F(1), initial condition n = 2
+
+	printf("%d %d ", F0, F1); //Display initial value  n<2 or n<=1
+
+	for (n; n <= m; n++)
+	{
+		Fn = Fn_2 + Fn_1; //F(n) = F(n-2) + F(n-1) Fibonacci recurrence relation
+		Fn_2 = Fn_1; //F(n-2) = F(n-1), recursive formula
+		Fn_1 = Fn; //F(n-1) = F(n), recursive formula
+		printf("%d ", Fn); //Display fibonacci sequence n>1 or n>=2
+	}
+
+	return 0;
+}
